933866f4c371e04a42787385b8bfb8ae96009fa2
Taehyeon Lim <plmoknijb3123@gmail.com>
2024-02-04 08:31:34 +0900

    Leetcode 274. H-Index - binary search solution

--- src/p274.py
+++ src/p274.py
@@ -17,13 +17,31 @@ class Solution:
         h_idx = 0
         for i, citation in enumerate(citations):
             if len(citations) - i <= citation:
-                h_idx = max(h_idx, len(citations) - i)
+                h_idx = len(citations) - i
+                break
+        return h_idx
+
+    def hIndex_sort_binary_search(self, citations: List[int]) -> int:
+        citations.sort()
+
+        h_idx = 0
+        n = len(citations)
+        start, end = 0, n - 1
+
+        while start <= end:
+            mid = (start + end) // 2
+            if n - mid <= citations[mid]:
+                h_idx = max(h_idx, n - mid)
+                end = mid - 1
+            else:
+                start = mid + 1
+
         return h_idx
 
 
 def test_solution():
     s = Solution()
 
-    assert s.hIndex_sort([3, 0, 6, 1, 5]) == 3
-    assert s.hIndex_sort([1, 3, 1]) == 1
-    assert s.hIndex_sort([100]) == 1
+    assert s.hIndex_sort_binary_search([3, 0, 6, 1, 5]) == 3
+    assert s.hIndex_sort_binary_search([1, 3, 1]) == 1
+    assert s.hIndex_sort_binary_search([100]) == 1
